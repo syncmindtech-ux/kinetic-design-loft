d71a89c30328b89af6e6cb08d93c3a7019d20a94
syncmindtech-ux <allan.kanoonya.ot@gmail.com>
2026-01-12 16:33:34 +0300

    Change alias path to empty string
    Updated alias path in Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [react()].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, ""),
     },
   },
 }));
